3a32813ef7216b7d67586f9852df4064506383b2
Alex Quirk <alex.quirk@telus.com>
2018-02-11 17:12:13 -0500

    Format client.js

--- src/app/client.js
+++ src/app/client.js
@@ -1,27 +1,32 @@
-/* eslint-disable */
+/* globals document */
+
 import React from 'react';
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware } from 'redux';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { ConnectedRouter, routerMiddleware } from 'react-router-redux'
-import createBrowserHistory from 'history/createBrowserHistory'
+import { ConnectedRouter, routerMiddleware } from 'react-router-redux';
+import createBrowserHistory from 'history/createBrowserHistory';
 
 import routes from './routes';
-import reducers from './reducers'
+import reducers from './reducers';
 
-const history = createBrowserHistory()
+const history = createBrowserHistory();
 
-const middleware = routerMiddleware(history)
+const middleware = routerMiddleware(history);
 
 const rootEl = document.getElementById('app');
 
-const store = createStore(
-  reducers,
-  applyMiddleware(middleware)
-)
+const store = createStore(reducers, applyMiddleware(middleware));
 
 /*
 configureStore takes 3 args, the root reducer, init store state, and additional middlewares
 by default, configureStore comes with redux-logger middleware. Here we
 registering our fetchMiddleware
 */
+
+ReactDOM.hydrate(
+  <Provider store={store}>
+    <ConnectedRouter history={history} routes={routes} />
+  </Provider>,
+  rootEl,
+);
